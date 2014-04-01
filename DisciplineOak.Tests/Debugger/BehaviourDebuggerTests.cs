015b93f495d283948937baabd1fe48241b4858c2
Michael O'Kane <ml_o_kane@hotmail.com>
2014-04-01 18:38:16 +0100

    Added proper support for debugging, allowing a user defined debugger to hook into all Ticking Tasks

---
+++ DisciplineOak.Tests/Debugger/BehaviourDebuggerTests.cs
@@ -0,0 +1,84 @@
+﻿using DisciplineOak.Debugger;
+using DisciplineOak.Execution.Core;
+using DisciplineOak.Model.Task.Composite;
+using DisciplineOak.Model.Task.Leaf.Action;
+using NUnit.Framework;
+using Moq;
+
+namespace DisciplineOak.Tests.Debugger
+{
+	[TestFixture]
+	public class BehaviourDebuggerTests
+	{
+		private Mock<IBehaviourDebugger> mockDebugger;
+
+		[SetUp]
+		public void Setup()
+		{
+			mockDebugger = new Mock<IBehaviourDebugger>();
+			BehaviourDebugger.SetActiveDebugger(mockDebugger.Object);
+		}
+
+		[Test]
+		public void When_Ticked_then_Task_Logs_Tick_with_Debugger()
+		{
+			var modelSequence = new ModelSequence(null,
+				new ModelOfAction<MyAction>(null),
+				new ModelOfAction<MyAction>(null));
+			var executor = BTExecutorFactory.CreateBTExecutor(modelSequence);
+
+			executor.Tick();
+			executor.Tick();
+			mockDebugger.Verify(m => m.LogTick(It.IsAny<MyAction>()), Times.Exactly(1));
+		}
+
+		[Test]
+		public void When_Ticked_then_Guard_Logs_Tick_with_Debugger()
+		{
+			var guard = new ModelOfAction<MyAction>(null);
+			
+			var modelSequence = new ModelDynamicPriorityList(null,
+				new ModelOfAction<MyAction>(guard),
+				new ModelOfAction<MyAction>(null));
+			var executor = BTExecutorFactory.CreateBTExecutor(modelSequence);
+
+			executor.Tick();
+			mockDebugger.ResetCalls();
+			guard.Action.ReturnStatusForInternalTick = Status.Success;
+			executor.Tick();
+			mockDebugger.Verify(m => m.LogTick(It.Is((ExecutionTask task) => task.ModelTask == guard)), Times.Exactly(1));
+		}
+
+		[Test]
+		public void When_Debugger_is_null_then_does_not_throw()
+		{
+			BehaviourDebugger.SetActiveDebugger(null);
+			var modelSequence = new ModelOfAction<MyAction>(null);
+			var executor = BTExecutorFactory.CreateBTExecutor(modelSequence);
+
+			Assert.DoesNotThrow(delegate
+			{
+				executor.Tick();
+				executor.Tick();
+			});
+
+			mockDebugger.Verify(m => m.LogTick(It.IsAny<MyAction>()), Times.Never);
+		}
+
+		[Test]
+		public void When_Debugger_Disabled_then_Logs_not_Received()
+		{
+			BehaviourDebugger.DisableDebugger();
+			var modelSequence = new ModelOfAction<MyAction>(null);
+			var executor = BTExecutorFactory.CreateBTExecutor(modelSequence);
+
+			Assert.DoesNotThrow(delegate
+			{
+				executor.Tick();
+				executor.Tick();
+			});
+
+			mockDebugger.Verify(m => m.LogTick(It.IsAny<MyAction>()), Times.Never);
+		}
+	}
+}
---
+++ DisciplineOak/Debugger/IBehaviourDebugger.cs
@@ -0,0 +1,9 @@
+﻿using DisciplineOak.Execution.Core;
+
+namespace DisciplineOak.Debugger
+{
+	public interface IBehaviourDebugger
+	{
+		void LogTick(ExecutionTask task);
+	}
+}
